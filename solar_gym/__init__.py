14f5c3dff8c2eedf4dcb1abf281cfeca3fdc8bb4
Chris Green <39661130+CGreen4u@users.noreply.github.com>
2020-06-25 12:47:28 -0500

    Update __init__.py

--- solar_gym/__init__.py
+++ solar_gym/__init__.py
@@ -1,1 +1,10 @@
+from gym.envs.registration import register
 
+register(
+    id='solar-v0',
+    entry_point='gym_solar.envs:SolarEnv',
+)
+register(
+    id='solar-extrahard-v0',
+    entry_point='gym_solar.envs:SolarExtraHardEnv',
+)
